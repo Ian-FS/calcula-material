8241a86f117e12f1fe4ca95af1160873579238b6
Ian_França <iiann_2009@hotmail.com>
2023-04-22 16:12:36 -0300

    atualizado a lógica e estilo da página

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import "./reset.scss";
 import "./style.scss";
+
+const form = document.getElementById("form");
+const inputConsumptionHdpePowder = document.getElementById("consumption-hdpe-powder");
+const inputConsumptionHdpeGranulated = document.getElementById("consumption-hdpe-granulated");
+const selectPowderGranulated = document.getElementById("select-powder-granulated");
+const capacidadeSistemaExtrusora = 200;
+
+// time = inputConsumptionHdpePowder * 200;
+
+form.children[2].style.display = "none";
+
+form.addEventListener("change", () => {
+  if (selectPowderGranulated.value === "granulated") {
+    form.children[1].style.display = "none";
+    form.children[2].style.display = "flex";
+  }
+  if (selectPowderGranulated.value === "powder") {
+    form.children[2].style.display = "none";
+    form.children[1].style.display = "flex";
+  }
+});
